b6988c3f1fa24bb866b6e9da15f605c9ba5c0803
Fazal Ashraf <fazalashrafofficial@gmail.com>
2023-11-04 18:41:06 +0530

    Changed weapon system
    Standard Shooting without weapon reload and bullets count

--- Assets/CharacterAiming.cs
+++ Assets/CharacterAiming.cs
@@ -6,10 +6,6 @@ using UnityEngine.Animations.Rigging;
 public class CharacterAiming : MonoBehaviour {
     public float turnSpeed = 15;
     public float aimDuration = 0.3f;
-    public float firerate = 0.0f;
-    public int maxAmmo = 30;
-    private int currentAmmo;
-    private bool isReloading = false;
 
     Camera mainCamera;
     public Rig aimLayer;
@@ -23,7 +19,6 @@ public class CharacterAiming : MonoBehaviour {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         weapon = GetComponentInChildren<RaycastWeapon>();
-        currentAmmo = maxAmmo;
     }
 
     // Update is called once per frame
@@ -33,8 +28,6 @@ public class CharacterAiming : MonoBehaviour {
     }
 
     private void LateUpdate() {
-        if (isReloading)
-            return;
 
         if (Input.GetMouseButton(1)) {
             aimLayer.weight += Time.deltaTime / aimDuration;
@@ -42,14 +35,11 @@ public class CharacterAiming : MonoBehaviour {
             aimLayer.weight -= Time.deltaTime / aimDuration;
         }
 
-        if (Input.GetButtonDown("Fire1") && aimLayer.weight ==1) {
-            if (currentAmmo > 0) {
-                isFiring = true;
-                    StartCoroutine(AutoFire());
-            } else {
-                isFiring = false;
-                StartCoroutine(Reload());
-            }
+        if (Input.GetButtonDown("Fire1") && aimLayer.weight == 1) {
+
+            isFiring = true;
+            weapon.StartFiring();
+
         }
 
         if (Input.GetButtonUp("Fire1")) {
@@ -58,27 +48,4 @@ public class CharacterAiming : MonoBehaviour {
         }
     }
 
-    // Coroutine for auto-firing
-    IEnumerator AutoFire() {
-        while (isFiring) {
-            if (currentAmmo > 0) {
-                weapon.StartFiring();
-                currentAmmo--;
-            } else {
-                isFiring = false; // Stop auto-firing when the magazine is empty
-                StartCoroutine(Reload());
-            }
-            yield return new WaitForSeconds(firerate);
-        }
-    }
-
-    IEnumerator Reload() {
-        if (isReloading || currentAmmo == maxAmmo)
-            yield break;
-        
-        isReloading = true;
-        yield return new WaitForSeconds(2.0f);
-        currentAmmo = maxAmmo;
-        isReloading = false;
-    }
 }
